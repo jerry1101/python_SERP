679b4c990c3bc7367f55fa64085da1514ca91b7f
jerry1101 <jerry110160@gmail.com>
2017-12-27 14:27:28 -0500

    1. remove image result 2. list first 5

--- serp.py
+++ serp.py
@@ -1,4 +1,4 @@
-from lxml import html
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,8 +8,13 @@ soup= BeautifulSoup(page.text,'html.parser')
 
 urls = soup.find_all('h3', class_='r')
 
+# remove image result
+for url in urls:
+	if 'Images' in url.text:
+		urls.remove(url)
 
-for search in urls[5:]:
-	print(search.text)
-	print("URL: ",search.a['href'])
+# list the first five
+for search in urls[:5]:
+		print(search.text)
+		print("URL: ",search.a['href'])
 
